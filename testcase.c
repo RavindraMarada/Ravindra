088e2a204d56a540e8a13811d33816e0296fb039
RAVINDRA MARADA <ravindra.phenom10@gmail.com>
2017-09-26 11:23:51 +0530

    my sample program

---
+++ testcase.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ void main(){
+      int a,b,sum=0,n;
+      scanf("%d",&n);
+      for(a=0;a<n;a++){
+         scanf("%d",&b);
+       sum=sum+b;
+}
+          printf("sum is %d",sum);
+}
